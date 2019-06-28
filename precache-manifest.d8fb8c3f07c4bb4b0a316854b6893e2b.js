7d828959221ced33094e209a4fe3797b7743094f
Anton <toxasava@icloud.com>
2019-06-27 23:51:13 -0400

    Updates

---
+++ precache-manifest.d8fb8c3f07c4bb4b0a316854b6893e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f2d6c8a7ef0f26c74fb90112d5104e",
+    "url": "/contact_keeper/index.html"
+  },
+  {
+    "revision": "cecfe9f15fc5e48d2899",
+    "url": "/contact_keeper/static/css/main.8a57dd70.chunk.css"
+  },
+  {
+    "revision": "b6146de6654bd4993821",
+    "url": "/contact_keeper/static/js/2.5d0e79c8.chunk.js"
+  },
+  {
+    "revision": "cecfe9f15fc5e48d2899",
+    "url": "/contact_keeper/static/js/main.ed9c7fa6.chunk.js"
+  },
+  {
+    "revision": "d60d95fc898c7157212d",
+    "url": "/contact_keeper/static/js/runtime~main.379abdcf.js"
+  }
+]);
